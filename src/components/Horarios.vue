384149431991b63a0662a6e741c428e0cd1383a2
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-28 13:38:04 -0600

    Creando esquema principal

---
+++ src/components/Horarios.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card flat>
+    <v-card-text>Horarios ahora sí</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Horarios"
+}
+</script>
---
+++ src/components/Rutas.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card flat>
+    <v-card-text>Rutas ahora sí</v-card-text>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi reprehenderit repudiandae sunt totam ullam!
+      Adipisci aperiam earum esse fugit, harum hic illo ipsum molestias odio perferendis praesentium quasi, repudiandae
+      suscipit!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut blanditiis distinctio eligendi odit quos. Commodi
+      consequatur, delectus dignissimos doloremque doloribus eveniet facere incidunt laudantium magni minima, sequi sunt
+      voluptate voluptatem?
+
+    </p>
+    <v-btn
+        color="primary"
+        fab
+        elevation="2"
+        right
+        bottom
+        fixed
+    >
+      a
+    </v-btn>
+    <br>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Rutas"
+}
+</script>
+
+<style scoped>
+
+</style>

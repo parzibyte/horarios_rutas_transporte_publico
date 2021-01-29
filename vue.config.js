a9cdadd47f467e62eb4e9fdca87f1fdec98d4f1f
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-28 20:44:38 -0600

    Preparando para producción

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    // En realidad debería ser "./" pero al compilar se remueve el ./ así que mejor se pone doble para que solo
+    // se remueva el primero. Gracias a: https://github.com/vuejs/vue-cli/issues/1623#issuecomment-729086789
+    publicPath: process.env.NODE_ENV === 'production' ? '././' : './',
+    transpileDependencies: [
+        'vuetify'
+    ]
 }
